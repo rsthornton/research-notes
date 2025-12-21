5518007a49074da10cbb295ce07e9e53d3e27b6f
rsthornton <rsthornton@gmail.com>
2025-12-21 17:57:18 -0500

    fix(explorer): hide glossary folder from sidebar (47 terms too overwhelming)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
     Component.Explorer({
-      filterFn: (node) => node.slugSegment !== "tags" && node.slugSegment !== "reflections",
+      filterFn: (node) => node.slugSegment !== "tags" && node.slugSegment !== "reflections" && node.slugSegment !== "glossary",
     }),
   ],
   right: [
@@ -64,7 +64,7 @@ export const defaultListPageLayout: PageLayout = {
       ],
     }),
     Component.Explorer({
-      filterFn: (node) => node.slugSegment !== "tags" && node.slugSegment !== "reflections",
+      filterFn: (node) => node.slugSegment !== "tags" && node.slugSegment !== "reflections" && node.slugSegment !== "glossary",
     }),
   ],
   right: [],
